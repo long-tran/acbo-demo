f714f46605450ca1478fda34c0c13832ee077332
Long Tran <ltran@long-tran.com>
2018-06-26 23:58:16 +0700

    add war and yml files

--- grails-app/controllers/acbo/ApiController.groovy
+++ grails-app/controllers/acbo/ApiController.groovy
@@ -1,15 +1,16 @@
 package acbo
 
 class ApiController {
+    static responseFormats = ['json', 'html']
     def loans () {
         def mem = new String[30000]
         sleep(Math.abs(new Random().nextInt() % 6000) + 1000)
         def bal = params.id.toLong() + 3893983
-        respond ['outstandingBalance': bal]
+        respond( ['outstandingBalance': bal] )
     }
 
     def casas () {
         def bal = params.id.toLong() + 7182932
-        respond ['availableBalance': bal]
+        respond( ['availableBalance': bal] )
     }
 }
